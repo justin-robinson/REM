3d800b13ce5b739a33009434f967aa8066d44b3c
jurobn <jurobn@amazon.com>
2016-12-01 11:31:18 -0800

    [test] add gulp js task

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var sassSource = './sass/*.scss';
+var cssOutput = './src/css';
+
 gulp.task('sass', function () {
-    return gulp.src('./sass/*.scss')
+    return gulp.src(sassSource)
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./src/css'));
+        .pipe(gulp.dest(cssOutput));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/*.scss', ['sass']);
+    gulp.watch(sassSource, ['sass']);
+});
+
+gulp.task('js', function () {
+    return gulp.src('./bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js')
+        .pipe(gulp.dest('./src/js/'));
 });
